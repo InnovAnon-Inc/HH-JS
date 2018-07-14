5a6d42226209417455e9fb760e57e0c0bd76eba0
ਬ੍ਰਹ੍ਮ੍ਜੋਤ ਸਿੰਘ ਖਾਲਸਾ <laurence.a.maddox@gmail.com>
2018-07-14 18:49:13 -0500

    Update hh.js

--- hh.js
+++ hh.js
@@ -1,5 +1,3 @@
-alert ("a");
-
 const IntervalToSemitones = {
   "P1" : 0,
   "m2" : 1,
@@ -15,22 +13,20 @@ const IntervalToSemitones = {
   "M7" : 11
 };
 
-alert ("c");
-/*
 const SemitonesToIntervals = {
-  0 : (P1),
-  1 : (m2),
-  2 : (M2),
-  3 : (m3),
-  4 : (M3),
-  5 : (P4),
-  6 : (A4, d5),
-  7 : (P5),
-  8 : (m6),
-  9 : (M6),
-  10 : (m7),
-  11 : (M7)
+  0 : ("P1"),
+  1 : ("m2"),
+  2 : ("M2"),
+  3 : ("m3"),
+  4 : ("M3"),
+  5 : ("P4"),
+  6 : ("A4", "d5"),
+  7 : ("P5"),
+  8 : ("m6"),
+  9 : ("M6"),
+  10 : ("m7"),
+  11 : ("M7")
 };
 
 alert ("success");
-*/
+
