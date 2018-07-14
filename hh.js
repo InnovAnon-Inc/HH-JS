aef908d769a230bb18f6172d8c995f2d56552acf
ਬ੍ਰਹ੍ਮ੍ਜੋਤ ਸਿੰਘ ਖਾਲਸਾ <laurence.a.maddox@gmail.com>
2018-07-14 17:11:15 -0500

    Create hh.js

---
+++ hh.js
@@ -0,0 +1,34 @@
+const enum Intervals = {
+  P1, m2, M2, m3, M3, P4, A4, d5, P5, m6, M6, m7, M7 
+};
+
+const IntervalToSemitones = {
+  P1 : 0,
+  m2 : 1,
+  M2 : 2,
+  m3 : 3,
+  M3 : 4,
+  P4 : 5,
+  A4 : 6, d5 : 6,
+  P5 : 7,
+  m6 : 8,
+  M6 : 9,
+  m7 : 10,
+  M7 : 11
+};
+
+const SemitonesToIntervals = {
+  0 : (P1),
+  1 : (m2),
+  2 : (M2),
+  3 : (m3),
+  4 : (M3),
+  5 : (P4),
+  6 : (A4, d5),
+  7 : (P5),
+  8 : (m6),
+  9 : (M6),
+  10 : (m7),
+  11 : (M7)
+};
+
