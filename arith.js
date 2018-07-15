e9c35c6efca7893e79987f50c2924fdcfe4ea6f7
ਬ੍ਰਹ੍ਮ੍ਜੋਤ ਸਿੰਘ ਖਾਲਸਾ <laurence.a.maddox@gmail.com>
2018-07-15 04:32:45 -0500

    Update arith.js

--- arith.js
+++ arith.js
@@ -1,11 +1,11 @@
 function intDiv (a, b) {
- var result = a / b;
+ const result = a / b;
  if (result >= 0)
   return Math.floor (result);
  else
   return Math.ceil (result);
 }
 
-function sum (array) {
- return array.reduce ((x, y) => x + y);
-}
+function sum (array) { return array.reduce ((x, y) => x + y); }
+
+function binaryLog (n) { return Math.log (n) / Math.log (2); }
